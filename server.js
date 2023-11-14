809518e2d12bfe9c68940c346cb659dabf6d14aa
Ghazali <aymenghazali1999@gmail.com>
2023-11-14 15:56:13 +0100

    morgan middleware & postman set-up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Route files :
 
@@ -12,9 +12,12 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// Mount routers
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(logger);
+// Mount routers
 
 app.use('/api/v1/bootcamps', bootcamps);
 
