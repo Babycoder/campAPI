48aae8eca1118be7a8b5e3c9a3f2cf138fbc3aee
Ghazali <aymenghazali1999@gmail.com>
2023-11-15 16:34:10 +0100

    colors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
-
+const colors = require('colors');
 // Load env vars :
 
 dotenv.config({ path: './config/config.env' });
@@ -28,7 +28,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle promise unhandled rejections
+
+process.on('unhandledRejection', (err, Promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => {
+    process.exit(1);
+  });
+});
